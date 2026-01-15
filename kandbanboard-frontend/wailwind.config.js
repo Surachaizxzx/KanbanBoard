f7abba02074aa15b059eb3ee660747aca4d614b9
koonguy <143887257+Surachaizxzx@users.noreply.github.com>
2026-01-15 10:07:46 +0700

    feat: init font natosans

---
+++ kandbanboard-frontend/wailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,js,jsx}", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Noto Sans Thai'", "ui-sans-serif", "system-ui"]
+      }
+    }
+  },
+  plugins: []
+};
